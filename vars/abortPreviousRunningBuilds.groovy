d0c5f69228dc755061f8df17af611de4a9e60b41
최동호 <wisdomcdh@gmail.com>
2019-11-10 23:23:56 +0900

    Update abortPreviousRunningBuilds.groovy

--- vars/abortPreviousRunningBuilds.groovy
+++ vars/abortPreviousRunningBuilds.groovy
@@ -2,18 +2,16 @@ import hudson.model.Result
 import jenkins.model.CauseOfInterruption
 
 def call() {
-  // current build number
-  def buildNumber = env.BUILD_NUMBER as int
-  
-  // iterate through current project runs
-  build.getProject()._getRuns().iterator().each{ run ->
-    def exec = run.getExecutor()
-    def execBuildNumber = build.getId() as int
-    // if the run is not a current build and it has executor (running) then stop it
-    if( run!=build && exec!=null && execBuildNumber<buildNumber){
-      //prepare the cause of interruption
-      def cause = { "interrupted by build #${execBuildNumber}" as String } as CauseOfInterruption 
-      exec.interrupt(Result.ABORTED, cause)
+    // current build number
+    def jobName = env.JOB_NAME
+    def currentBuildNumber = env.BUILD_NUMBER.toInteger()
+    def currentJob = Jenkins.instance.getItemByFullName(jobName)
+
+    for(def build : currentJob.builds) {
+        if(build.isBuilding() && build.number.toInteger() < currentBuildNumber) {
+            def exec = build.getExecutor();
+            def cause = new CauseOfInterruption.UserInterruption("interrupted by build #${currentBuildNumber}")
+            exec.interrupt(Result.ABORTED, cause)
+        }
     }
-  }
 }
