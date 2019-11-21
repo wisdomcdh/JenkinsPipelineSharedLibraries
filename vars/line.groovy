b2aad90b1b9a68717301c4cccd2ff4da134ef374
최동호 <wisdomcdh@gmail.com>
2019-11-21 10:15:28 +0900

    Update line.groovy

--- vars/line.groovy
+++ vars/line.groovy
@@ -14,19 +14,17 @@ def notify(maps) {
   def postdata = ''
   postdata += 'message=' + maps.get('message')
   if(maps.containsKey('imageThumbnail')) {
-    postdata += '&imageThumbnail=' + maps.get('imageThumbnail')
+    postdata += '&imageThumbnail=' + java.net.URLEncoder.encode(maps.get('imageThumbnail'), 'UTF-8')
   }
   if(maps.containsKey('imageFullsize')) {
-    postdata += '&imageFullsize=' + maps.get('imageFullsize')
+    postdata += '&imageFullsize=' + java.net.URLEncoder.encode(maps.get('imageFullsize'), 'UTF-8')
   }
   if(maps.containsKey('stickerPackageId')) {
-    postdata += '&stickerPackageId=' + maps.get('stickerPackageId')
+    postdata += '&stickerPackageId=' + java.net.URLEncoder.encode(maps.get('stickerPackageId'), 'UTF-8')
   }
   if(maps.containsKey('notificationDisabled')) {
-    postdata += '&notificationDisabled=' + maps.get('notificationDisabled')
+    postdata += '&notificationDisabled=' + java.net.URLEncoder.encode(maps.get('notificationDisabled'), 'UTF-8')
   }
-  
-  postdata = java.net.URLEncoder.encode(postdata, 'UTF-8')
      
   withCredentials([string(credentialsId: maps.get('access_token'), variable: 'LINE_NOTI_TOKEN')]) {
       // 문자열의 표현식을 바꾸려면 작은 따옴표 대신 큰 따옴표를 사용해야 합니다.
