abb5f73c1ae19b3331b2bec428fbf84ade317b59
최동호 <wisdomcdh@gmail.com>
2020-04-11 12:11:25 +0900

    Create bitbucket.groovy

---
+++ vars/bitbucket.groovy
@@ -0,0 +1,27 @@
+def branches(maps) {
+  /* maps
+  [
+  access_key: '',
+  workspace: '',
+  repo_slug: '',
+  q: ''
+  ]
+  */
+  def workspace = maps.get('workspace')
+  def repo_slug = maps.get('repo_slug')
+  def q = maps.get('q')
+  if (q == null) {
+    q = 'q=name ~ "CKDEV-1"'
+  }
+
+  def result;
+  withCredentials([string(credentialsId: maps.get('access_key'), variable: 'CURL_USER')]) {
+    result = sh (
+      script '''
+        curl -s -G --data-urlencode '${q}' -u '${CURL_USER}' https://api.bitbucket.org/2.0/repositories/${workspace}/${repo_slug}/refs/branches | jq '.values [] | .name'
+      '''
+      returnStdout: true
+    )
+  }
+  return result.split('\n');
+}
