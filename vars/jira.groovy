f0adad95777f9342f38d45d9f0bfdce1a269e25f
최동호 <wisdomcdh@gmail.com>
2020-04-12 20:43:25 +0900

    Update jira.groovy

--- vars/jira.groovy
+++ vars/jira.groovy
@@ -11,11 +11,15 @@ def addComment(maps) {
 
   def result;
   withCredentials([string(credentialsId: maps.get('access_key'), variable: 'CURL_USER')]) {
-    sh (
+    result = sh (
       script: """
         curl -s -X POST -u '${CURL_USER}' -H 'Content-Type: application/json' -d '${body}' https:///chalcak.atlassian.net/rest/api/2/issue/${issue_key}/comment
-      """
+      """,
+      returnStatus: true
     )
+  }
+  if(result != 0) {
+    throw new Exception("addComment fail");
   }
 }
 
@@ -32,10 +36,14 @@ def editIssue(maps) {
 
   def result;
   withCredentials([string(credentialsId: maps.get('access_key'), variable: 'CURL_USER')]) {
-    sh (
+    result = sh (
       script: """
         curl -s -X PUT -u '${CURL_USER}' -H 'Content-Type: application/json' -d '${body}' https:///chalcak.atlassian.net/rest/api/2/issue/${issue_key}
-      """
+      """,
+      returnStatus: true
     )
+  }
+  if(result != 0) {
+    throw new Exception("editIssue fail");
   }
 }
