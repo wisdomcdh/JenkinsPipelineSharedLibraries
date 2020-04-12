10f639971e6c1555aacf6dc97e2647b4f7f312f2
최동호 <wisdomcdh@gmail.com>
2020-04-12 18:39:27 +0900

    Update jira.groovy

--- vars/jira.groovy
+++ vars/jira.groovy
@@ -2,6 +2,7 @@ def addComment(maps) {
   /* maps
   [
   access_key: '',
+  issue_key: '',
   body: ''
   ]
   */
@@ -13,6 +14,27 @@ def addComment(maps) {
     sh (
       script: """
         curl -s -X POST -u '${CURL_USER}' -H 'Content-Type: application/json' -d '${body}' https:///chalcak.atlassian.net/rest/api/2/issue/${issue_key}/comment
+      """
+    )
+  }
+}
+
+def editIssue(maps) {
+  /* maps
+  [
+  access_key: '',
+  issue_key: '',
+  body: ''
+  ]
+  */
+  def issue_key = maps.get('issue_key')
+  def body = maps.get('body')
+
+  def result;
+  withCredentials([string(credentialsId: maps.get('access_key'), variable: 'CURL_USER')]) {
+    sh (
+      script: """
+        curl -s -X PUT -u '${CURL_USER}' -H 'Content-Type: application/json' -d '${body}' https:///chalcak.atlassian.net/rest/api/2/issue/${issue_key}
       """
     )
   }
